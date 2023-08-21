3ddb6784c3d3e7820a2fa5d06575ba575373e36b
unknown <ansariawaisraees@gmail.com>
2023-08-22 00:16:08 +0530

    Adding Solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,8 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price = isVeg ? 300 : 400;
+        this.bill = "Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +19,26 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.price += 80;
+        this.bill += "Extra Cheese Added: 80\n";
     }
 
     public void addExtraToppings(){
         // your code goes here
+        int toppingsPrice = isVeg ? 70 : 120;
+        this.price += toppingsPrice;
+        this.bill += "Extra Toppings Added: "+toppingsPrice+"\n";
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price += 20;
+        this.bill += "Paperbag Added: 20\n";
     }
 
     public String getBill(){
         // your code goes here
+        this.bill += "Total Price: "+this.price;
         return this.bill;
     }
 }
